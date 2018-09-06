ce2d4a4e0bea510f177a1d285a43233585486bd3
xiaohuimc <xiaohuimc@hotmail.com>
2018-09-06 13:27:19 +0800

    2018-9-6

--- Zoomeye.py
+++ Zoomeye.py
@@ -3,36 +3,32 @@
 import requests
 import json
 import time
+import getopt
+import sys
 
-def getJson(Keyword,Bcountry,pageNumber):
+headers = {
+    "Host": "www.zoomeye.org",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-GB,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Cube-Authorization": "修改此处",
+    "Cookie": "修改此处",
+    "Connection": "keep-alive",
+    "Cache-Control": "max-age=0"
+}
 
-    url='https://www.zoomeye.org/api/search?q={} +country:"{}"&t=all&p={}'.format(Keyword,Bcountry,pageNumber)
-    headers={
-        "Host": "www.zoomeye.org",
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0",
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "en-GB,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Referer": "https://www.zoomeye.org/searchResult?q={}%20%2Bcountry:%22{}%22&t=all".format(Keyword,Bcountry),
-        "Cube-Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjkwODc4MzY0N0BxcS5jb20iLCJ1dWlkIjoiY2I1MTg2NGI4ZTJhOThkMDdkNWI4NzM3YWQwNTc0YTAiLCJpYXQiOjE1MzU0NTI3NzIsImV4cCI6MTUzNTUzOTE3Mn0.h7aHKFYj21fsLCHZoUxnog4PcQahSFWedwqi19lN1bU",
-        "Cookie": "__jsluid=eb52d6a6aa569c576703f7b2e405e035; Hm_lvt_3c8266fabffc08ed4774a252adcb9263=1535361030,1535361060,1535452769; __jsl_clearance=1535452765.973|0|hrEYpghty%2FoY3bD6di1a7NEWsok%3D; Hm_lpvt_3c8266fabffc08ed4774a252adcb9263=1535452772",
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0"
-    }
-    s = requests.Session()
+def getJson(application,port,pageNumber):
 
-
-    try:
-        req=s.get(url, headers=headers)
-        if req.cookies.get_dict():
-            s.cookies.update(req.cookies)
-    except Exception, e:
-        print e
-    #r=requests.get(url,headers=headers)
-    #print(r.text)
-    return req.text
+    url='https://www.zoomeye.org/api/search?q=app:"{}" +port:"{}"&t=host&p={}'.format(application,port,pageNumber)
+    global headers
+    headers["Referer"] = "https://www.zoomeye.org/searchResult?q=app%3A%22{}%22%20%2Bport%3A%22{}%22&t=host".format(application,port)
+    s = requests.session()
+    r=s.get(url,headers=headers)
+    return r.text
 
 def wirteText(content):
+
     for i in json.loads(content)['matches']:
         f=open('ips.txt','a+')
         if type(i['ip'])==str:
@@ -44,19 +40,55 @@ def wirteText(content):
         else:
             pass
 
-def readJson():
-    f=open('geo.json', 'r')
-    jsons=json.load(f)
-    for x in jsons:#循环列出国家
-        print("===================={}=======================".format(x))
+def readJson(application,port):
+
+    try:
         for i in range(1, 101):  # 循环100次，页码自增1
             time.sleep(5)
-            content =getJson('weblogic', x, i)
-            wirteText(content)
+            content = getJson(application, port, i)
+            if json.loads(content)["status"] != 200:
+                print(json.loads(content)["msg"])
+                break
+            elif json.loads(content)["status"] == 200:
+                wirteText(content)
+    except json.decoder.JSONDecodeError:
+        print('Cookies has expired.')
 
-def run():
-    readJson()
+
+def run(argv):
+
+    application=''
+    port=''
+    try:
+        if len(argv)==4:
+            opts, args = getopt.getopt(argv, "ha:p:")
+        else:
+            print('Python Zoomeye.py -a <application> -p <port>')
+    except getopt.GetoptError:
+        print('Python Zoomeye.py -a <application> -p <port>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Python Zoomeye.py -a <application> -p <port>')
+            sys.exit()
+        elif opt in ("-a"):
+            application = arg
+        elif opt in ("-p"):
+            port = arg
+    readJson(application,port)
 
 if __name__ == '__main__':
-    run()
-    print('Crawl completion')
+
+
+    print("""
+    
+ _____
+/__  /  ____  ____  ____ ___  ___  __  _____
+  / /  / __ \/ __ \/ __ `__ \/ _ \/ / / / _ \\
+ / /__/ /_/ / /_/ / / / / / /  __/ /_/ /  __/
+/____/\____/\____/_/ /_/ /_/\___/\__, /\___/
+                                /____/
+     
+    """)
+    run(sys.argv[1:])
+
